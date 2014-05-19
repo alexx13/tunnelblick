alexx13/tunnelblick

commit 89f944e5a8ac056376cf055e5358eef601586db9

Build 3851: Updates connection status and statistics while waiting for the user to enter a password or passphrase and in most other situations.
git-svn-id: http://tunnelblick.googlecode.com/svn/trunk@2851 ecaf4b9d-c145-0410-a236-1b700048ef5c

tunnelblick/TBUIUpdater.h.m
@@ -1,0 +1,220 @@
+/*
+ * Copyright (c) 2014 Jonathan K. Bullard. All rights reserved.
+ *
+ *  This file is part of Tunnelblick.
+ *
+ *  Tunnelblick is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2
+ *  as published by the Free Software Foundation.
+ *
+ *  Tunnelblick is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (see the file COPYING included with this
+ *  distribution); if not, write to the Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  or see http://www.gnu.org/licenses/.
+ */
+
+//*************************************************************************************************
+/*
+ *  TBUIUpdater
+ *
+ *  TBUIUpdater is used to control UI elements that should update every second:
+ *
+ *      * TB menu items that display the time the status has been unchanged (and not disconnected)
+ *      * Notification window status, statistics, and the time the status has been unchanged (and not disconnected)
+ *      * The VPN Details window title for the selected configuration
+ *      * The Advanced window title for the selected configuration
+ *
+ */
+
+//*************************************************************************************************
+
+#import "TBUIUpdater.h"
+
+#import "NSTimer+TB.h"
+
+#import "helper.h"
+
+#import "MenuController.h"
+#import "MyPrefsWindowController.h"
+#import "SettingsSheetWindowController.h"
+#import "StatusWindowController.h"
+#import "TBUserDefaults.h"
+#import "VPNConnection.h"
+
+extern TBUserDefaults * gTbDefaults;
+
+@implementation TBUIUpdater
+
+-(id) init {
+    
+    self = [super init];
+    if (self) {
+		
+		invalidateAfterNextTickFlag = FALSE;
+		
+        NSTimeInterval period = [gTbDefaults timeIntervalForKey: @"displayUpdateInterval"
+                                                        default: 1.0
+                                                            min: 0.5
+                                                            max: 10.0];
+		
+        timer = [[NSTimer timerWithTimeInterval: period
+										 target: self selector:@selector(timerTickHandler:)
+									   userInfo: nil
+										repeats: YES]
+				 retain];
+
+		[[NSRunLoop mainRunLoop] addTimer: timer
+                                  forMode: (  runningOnLeopardOrNewer()
+                                            ? NSRunLoopCommonModes
+                                            : NSDefaultRunLoopMode)];
+        
+        [timer tbSetTolerance: -1.0];
+        TBLog(@"DB-UU", @"TBUIUpdater created with update interval %f", period);
+    }
+    
+    return self;
+}
+
+-(void) dealloc {
+    
+    [timer invalidate]; [timer release]; timer = nil;
+    
+    [nonDisconnectedConnectionsAtPreviousTick release]; nonDisconnectedConnectionsAtPreviousTick = nil;
+
+    TBLog(@"DB-UU", @"TBUIUpdater removed");
+
+    [super dealloc];
+}
+
+-(void) invalidateAfterNextTick {
+	
+	invalidateAfterNextTickFlag = TRUE;
+}
+
+-(void) fireTimer {
+	
+	[timer fire];
+}
+
+-(void) timerTickHandler: (NSTimer *) theTimer {
+    if (  timer == theTimer  ) {
+        
+        TBLog(@"DB-UU", @"TBUIUpdater timerTickHandler invoked");
+        
+        // Make a list of connections to update, consisting of
+        //      Connections that are not currrently disconnected
+        // plus
+        //      Connections that have been removed since the last tick
+        
+        MenuController * mc = [NSApp delegate];
+        NSArray * notDisconnected = [mc nondisconnectedConnections];
+        
+        NSUInteger maxSize = [notDisconnected count] + [nonDisconnectedConnectionsAtPreviousTick count];
+        NSMutableArray * connectionsToUpdate = [NSMutableArray arrayWithCapacity: maxSize];
+        [connectionsToUpdate addObjectsFromArray: notDisconnected];
+        NSEnumerator * oldListEnum = [nonDisconnectedConnectionsAtPreviousTick objectEnumerator];
+        VPNConnection * connection;
+        while (  (connection = [oldListEnum nextObject])  ) {
+            if (  ! [notDisconnected containsObject: connection]  ) {
+                [connectionsToUpdate addObject: connection];
+            }
+        }
+        
+        // Remember the non-disconnected connections for next time through
+        [self setNonDisconnectedConnectionsAtPreviousTick: notDisconnected];
+        
+        // Update the VPN Details... window and the Advanced window if one of the connections to update is selected
+        MyPrefsWindowController       * vpnDetailsWc = [mc logScreen];
+        SettingsSheetWindowController * advancedWc   = [vpnDetailsWc settingsSheetWindowController];
+        connection = [vpnDetailsWc selectedConnection];
+        if (   [connectionsToUpdate containsObject: connection]  ) {
+            NSWindow * vpnDetailsWindow = [vpnDetailsWc window];
+            NSWindow * advancedWindow   = [advancedWc window];
+			BOOL showingAdvancedWindow  = [advancedWc showingSettingsSheet];
+			if (   vpnDetailsWindow
+				|| showingAdvancedWindow  ) {
+				NSString * name = [connection displayName];
+				NSString * privateSharedDeployed = [connection displayLocation];
+				NSString * state = localizeNonLiteral([connection state], @"Connection status");
+				NSString * timeString = [connection connectTimeString];
+				if (  vpnDetailsWindow  ) {
+					NSString * statusMsg = [NSString stringWithFormat: @"%@%@: %@%@ - Tunnelblick",
+											name, privateSharedDeployed, state, timeString];
+					[vpnDetailsWindow setTitle: statusMsg];
+					TBLog(@"DB-UU", @"TBUIUpdater timerTickHandler: set title of VPN Details... window to '%@'", statusMsg);
+				}
+				if (   advancedWindow
+					&& showingAdvancedWindow  ) {
+					NSString * statusMsg = [NSString stringWithFormat: @"%@%@: %@%@ - Advanced - Tunnelblick",
+											name, privateSharedDeployed, state, timeString];
+					[advancedWindow setTitle: statusMsg];
+					TBLog(@"DB-UU", @"TBUIUpdater timerTickHandler: set title of Advanced       window to '%@'", statusMsg);
+				}
+			}
+		}
+        
+        // Update status windows and menu items for the connections to update
+        NSEnumerator * e = [connectionsToUpdate objectEnumerator];
+        while (  (connection = [e nextObject])  ) {
+			
+			StatusWindowController * swc = [connection statusScreen];
+			if (  [swc isOpen]  ) {
+				TBLog(@"DB-UU", @"TBUIUpdater timerTickHandler: updating status window for            '%@'", [connection displayName]);
+				[connection updateStatisticsDisplay];
+			}
+			
+			if (  [mc menuIsOpen]  ) {
+				TBLog(@"DB-UU", @"TBUIUpdater timerTickHandler: updating menu item                    '%@'", [connection displayName]);
+				NSMenuItem * menuItem = [connection menuItem];
+				[connection validateMenuItem: menuItem];
+			}
+		}
+		
+		if (  invalidateAfterNextTickFlag  ) {
+			[timer invalidate];
+		}
+    }
+}
+
+TBSYNTHESIZE_OBJECT(retain, NSArray *, nonDisconnectedConnectionsAtPreviousTick, setNonDisconnectedConnectionsAtPreviousTick)
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

tunnelblick/English.lproj/Localizable.strings
@@ -416,40 +416,49 @@
 "Double-click to begin" = "Double-click to begin";
 
 /* Connection status */
-"ADD_ROUTES" = "Adding routes";
+"ADD_ROUTES" = "Adding routes";   // Please translate "Adding routes"
 
 /* Connection status */
-"ASSIGN_IP" = "Assigning IP address";
+"ASSIGN_IP" = "Assigning IP address";   // Please translate "Assigning IP address"
 
 /* Connection status */
-"AUTH" = "Authorizing";
+"AUTH" = "Authorizing";   // Please translate "Authorizing"
 
 /* Connection status */
-"CONNECTED" = "Connected";
+"CONNECTED" = "Connected";   // Please translate "Connected"
 
 /* Connection status */
-"CONNECTING" = "Connecting";
+"CONNECTING" = "Connecting";   // Please translate "Connecting"
+
+/* Connection status */
+"DISCONNECTING" = "Disconnecting";     // Please translate "Disconnecting"
 
 /* Connection status */
 "EXITING" = "Disconnected";     // Please translate "Disconnected"
 
 /* Connection status */
-"GET_CONFIG" = "Getting configuration";
+"GET_CONFIG" = "Getting configuration";   // Please translate ""
 
 /* Connection status */
-"RECONNECTING" = "Reconnecting";
+"PASSWORD_WAIT" = "Waiting for password";   // Please translate "Waiting for password"
 
 /* Connection status */
-"RESOLVE" = "Resolving domain name";
+"PRIVATE_KEY_WAIT" = "Waiting for passphrase";   // Please translate "Waiting for password"
+
+/* Connection status */
+"RECONNECTING" = "Reconnecting";   // Please translate ""
+
+/* Connection status */
+"RESOLVE" = "Resolving domain name";   // Please translate ""
 
 /* Connection status */
 "SLEEP" = "Waiting for Daemon process";   // Please translate "Waiting for daemon process"
 
 /* Connection status */
-"TCP_CONNECT" = "Making TCP connection";
+"TCP_CONNECT" = "Making TCP connection";   // Please translate "Making TCP connection"
 
 /* Connection status */
-"UDP_CONNECT" = "Making UDP connection";
+"UDP_CONNECT" = "Making UDP connection";   // Please translate "Making UDP connection"
 
 /* Connection status */
 "WAIT" = "Waiting for server response";   // Please translate "Waiting for Server Response"
@@ -785,7 +794,7 @@
 "'%@' already exists on the Desktop.\n\nWould you like to replace it?" = "'%@' already exists on the Desktop.\n\nWould you like to replace it?";
 
 /* Window text */
-"'%@' does not have any credentials (private key or username and password) stored in the Keychain." = "'%@' does not have any credentials (private key or username and password) stored in the Keychain.";
+"'%@' does not have any credentials (passphrase or username and password) stored in the Keychain." = "'%@' does not have any credentials (passphrase or username and password) stored in the Keychain.";
 
 /* Window text */
 "(Never checked)" = "(Never checked)";
@@ -857,10 +866,10 @@
 "Are you sure you do not want to convert OpenVPN configurations to Tunnelblick VPN Configurations?\n\nCONFIGURATIONS WILL NOT BE AVAILABLE IF YOU DO NOT CONVERT THEM!\n\n" = "Are you sure you do not want to convert OpenVPN configurations to Tunnelblick VPN Configurations?\n\nCONFIGURATIONS WILL NOT BE AVAILABLE IF YOU DO NOT CONVERT THEM!\n\n";
 
 /* Window text */
-"Are you sure you wish to delete the credentials (private key or username and password) for '%@' that are stored in the Keychain?" = "Are you sure you wish to delete the credentials (private key or username and password) for '%@' that are stored in the Keychain?";
+"Are you sure you wish to delete the credentials (passphrase or username and password) for '%@' that are stored in the Keychain?" = "Are you sure you wish to delete the credentials (passphrase or username and password) for '%@' that are stored in the Keychain?";
 
 /* Window text */
-"Are you sure you wish to delete the credentials (private key or username and password) stored in the Keychain for '%@' credentials?" = "Are you sure you wish to delete the credentials (private key or username and password) stored in the Keychain for '%@' credentials?";
+"Are you sure you wish to delete the credentials (passphrase or username and password) stored in the Keychain for '%@' credentials?" = "Are you sure you wish to delete the credentials (passphrase or username and password) stored in the Keychain for '%@' credentials?";
 
 /* Window text */
 "B" = "B";

tunnelblick/MenuController.h
@@ -36,6 +36,7 @@
 @class SplashWindowController;
 @class StatusWindowController;
 @class SUUpdater;
+@class TBUIUpdater;
 @class UKKQueue;
 @class VPNConnection;
 @class WelcomeController;
@@ -113,6 +114,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
     NSDictionary            * myVPNConnectionDictionary;    // List of all VPNConnections. key = display name, value = VPNConnection object for the configuration
     
     NSArray                 * connectionArray;              // VPNConnections that are currently connected
+    NSArray                 * nondisconnectedConnections;   // VPNConnections that are currently not disconnected (any with status != EXITING)
     
     NSMutableArray          * connectionsToRestoreOnWakeup; // VPNConnections to be restored when awakened from sleep
     
@@ -127,7 +129,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
     
     UKKQueue                * myQueue;                      // UKKQueue item for monitoring the configuration file folder
     
-    NSTimer                 * showDurationsTimer;           // Used to periodically update display of connections' durations in the VPNDetails... Window
+    TBUIUpdater             * uiUpdater;                    // Used to periodically update displays
 	
     NSTimer                 * hookupWatchdogTimer;          // Used to check for failures to hookup to openvpn processes, and deal with unknown OpenVPN processes 
 	
@@ -196,7 +198,8 @@ void * _NSConcreteStackBlock __attribute__((weak));
 -(IBAction)         quit:                                   (id)                sender;
 
 // General methods
--(void)             addConnection:                          (id)                sender;
+-(void)             addConnection:                          (VPNConnection *)   connection;
+-(void)             addNonconnection:                       (VPNConnection *)   connection;
 -(void)             addNewConfig:                           (NSString *)        path
                  withDisplayName:                           (NSString *)        dispNm;
 -(void)             setPreferenceForSelectedConfigurationsWithKey: (NSString *) key
@@ -242,7 +245,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
 -(BOOL)             mouseIsInsideAnyView;
 -(NSString *)       openVPNLogHeader;
 -(void)             reconnectAfterBecomeActiveUser;
--(void)             removeConnection:                       (id)                sender;
+-(void)             removeConnection:                       (VPNConnection *)   connection;
 -(BOOL)             runInstaller:                           (unsigned)          installerFlags
                   extraArguments:                           (NSArray *)         extraArguments;
 
@@ -267,6 +270,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
 -(void)             hideStatisticsWindows;
 -(void)             updateIconImage;
 -(void)				updateMenuAndDetailsWindow;
+-(void)             updateUI;
 -(void)				updateUpdateFeedURLForceDowngrade:		(BOOL)				forceDowngrade;
 -(void)             terminateBecause:                       (enum TerminationReason) reason;
 
@@ -289,7 +293,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
 -(NSImage *)        largeMainImage;
 -(MyPrefsWindowController *) logScreen;
 -(NSString *)       customRunOnConnectPath;
--(void)             startOrStopDurationsTimer;
+-(void)             startOrStopUiUpdater;
 -(BOOL)             terminatingAtUserRequest;
 -(SUUpdater *)      updater;
 -(BOOL)				doingSetupOfUI;
@@ -311,6 +315,7 @@ void * _NSConcreteStackBlock __attribute__((weak));
 -(NSArray *)        applescriptConfigurationList;
 
 TBPROPERTY_READONLY(NSStatusItem *, statusItem)
+TBPROPERTY_READONLY(BOOL, menuIsOpen)
 TBPROPERTY_READONLY(NSMenu *,		myVPNMenu)
 TBPROPERTY_READONLY(NSMutableArray *, activeIPCheckThreads)
 TBPROPERTY_READONLY(NSMutableArray *, cancellingIPCheckThreads)
@@ -322,8 +327,9 @@ TBPROPERTY(NSDictionary *, myConfigDictionary,        setMyConfigDictionary)
 TBPROPERTY(NSArray      *, openvpnVersionNames,       setOpenvpnVersionNames)
 TBPROPERTY(NSArray      *, openvpnVersionInfo,        setOpenvpnVersionInfo)
 TBPROPERTY(NSArray      *, connectionArray,           setConnectionArray)
+TBPROPERTY(NSArray      *, nondisconnectedConnections,setNondisconnectedConnections)
 TBPROPERTY(NSTimer      *, hookupWatchdogTimer,       setHookupWatchdogTimer)
-TBPROPERTY(NSTimer      *, showDurationsTimer,        setShowDurationsTimer)
+TBPROPERTY(TBUIUpdater  *, uiUpdater,                 setUiUpdater)
 TBPROPERTY(NSTimer      *, configsChangedTimer,       setConfigsChangedTimer)
 TBPROPERTY(NSTimer      *, statisticsWindowTimer,     setStatisticsWindowTimer)
 TBPROPERTY(NSMutableArray *, highlightedAnimImages,   setHighlightedAnimImages)

commit 1484bdaf0a3eb211dc4991203d6a6ee7db4ffa52

Build 3852: Fixes a problem on OS X 10.4 ('Tiger') introduced in r2851

tunnelblick/TBUIUpdater.h.m
@@ -63,17 +63,18 @@ extern TBUserDefaults * gTbDefaults;
                                                             min: 0.5
                                                             max: 10.0];
 		
-        timer = [[NSTimer timerWithTimeInterval: period
-										 target: self selector:@selector(timerTickHandler:)
-									   userInfo: nil
-										repeats: YES]
+		timer = [[NSTimer scheduledTimerWithTimeInterval: period
+												  target: self selector:@selector(timerTickHandler:)
+												userInfo: nil
+												 repeats: YES]
 				 retain];
+		
+		if (  runningOnLeopardOrNewer()  ) {
+			[[NSRunLoop mainRunLoop]    addTimer: timer forMode: NSRunLoopCommonModes];
+		} else {
+			[[NSRunLoop currentRunLoop] addTimer: timer forMode: NSDefaultRunLoopMode];
+		}
 
-		[[NSRunLoop mainRunLoop] addTimer: timer
-                                  forMode: (  runningOnLeopardOrNewer()
-                                            ? NSRunLoopCommonModes
-                                            : NSDefaultRunLoopMode)];
-        
         [timer tbSetTolerance: -1.0];
         TBLog(@"DB-UU", @"TBUIUpdater created with update interval %f", period);
     }